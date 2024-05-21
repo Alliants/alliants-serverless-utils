b78dee6290290ca4a9e9cafc37dca1c4e8e6dd82
Martín Acosta <tinchoz49@gmail.com>
2024-05-21 12:38:35 -0300

    disable test container (#8)
    * disable test container
    
    * disable test container
    
    * disable test container
    
    * more changes
    
    * last change

--- src/plugins/offline-sqs/plugin.js
+++ src/plugins/offline-sqs/plugin.js
@@ -1,24 +1,35 @@
 /** @typedef {import('serverless')} Serverless */
 
+import process from 'node:process'
+
 import { CreateQueueCommand, SQSClient } from '@aws-sdk/client-sqs'
 import ServerlessSQSOffline from 'serverless-offline-sqs'
 import { GenericContainer } from 'testcontainers'
 
 export default class OfflineSQS extends ServerlessSQSOffline {
+  constructor(...args) {
+    super(...args)
+
+    if ('serverless-offline-sqs' in this.serverless.service.custom) {
+      // disable the autoCreate
+      this.serverless.service.custom['serverless-offline-sqs'].autoCreate = false
+    }
+  }
+
   async start() {
     const { endpoint, region } = this.serverless.service.custom['serverless-offline-sqs']
     const port = endpoint.split(':').pop()
-    this.elasticContainer = await new GenericContainer('softwaremill/elasticmq-native')
-      .withExposedPorts({
-        host: port,
-        container: 9324,
-      }, 9325)
-      .withName(`${this.serverless.service.service}-sqs`)
-      .withReuse()
-      .start()
 
-    // eslint-disable-next-line no-console
-    console.log(`> offline-sqs: http://localhost:${this.elasticContainer.getMappedPort(9325)}`)
+    if (!(process.env.DISABLE_TEST_CONTAINER === '1' || process.env.DISABLE_TEST_CONTAINER === 'true')) {
+      this.elasticContainer = await new GenericContainer('softwaremill/elasticmq-native')
+        .withExposedPorts({
+          host: port,
+          container: 9324,
+        }, 9325)
+        .withName(`${this.serverless.service.service}-sqs`)
+        .withReuse()
+        .start()
+    }
 
     const client = new SQSClient({
       region,
@@ -53,12 +64,13 @@ export default class OfflineSQS extends ServerlessSQSOffline {
     }
 
     await super.start()
+
+    // eslint-disable-next-line no-console
+    console.log(`> offline-sqs: http://localhost:${this?.elasticContainer?.getMappedPort(9325) || 9325}`)
   }
 
   async end() {
-    if (this.elasticContainer) {
-      await this.elasticContainer.stop()
-    }
+    await this?.elasticContainer?.stop()
     await super.end()
   }
 }
